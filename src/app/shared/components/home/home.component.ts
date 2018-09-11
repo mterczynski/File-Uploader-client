c368cc03e0c5377d0ec3e3b6497d32d4869b7484
Michał Terczyński <mterczynski271@gmail.com>
2018-09-11 20:45:44 +0200

    styling: removed unused code from home.component.ts

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
-declare let $: any; // jquery for bootstrap
 
 @Component({
   selector: 'app-home',
@@ -10,45 +9,16 @@ declare let $: any; // jquery for bootstrap
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('fileInput') fileInput;
-
-  isModalVisible = false;
-  isModalAnimationVisible = false;
-  selectedFiles: File[] = [];
-  selectedFileIndex = 3;
-  fileList = [];
-  serverUrl = environment.apiUrl;
+  private fileList = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get(this.serverUrl + '/files').subscribe((data) => {
-      this.fileList = <any[]>data;
+    this.http.get<any[]>(environment.apiUrl + '/files').subscribe((data) => {
+      this.fileList = data;
       this.fileList.forEach((file) => {
-        // const offset = 60000 * (new Date().getTimezoneOffset());
         file.uploadDate = new Date(new Date(file.uploadDate).getTime());
       });
     });
-  }
-
-  comparePaginatorIndex(index: number) {
-    if (index === this.selectedFileIndex) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  showModal() {
-    $('#modal').modal();
-  }
-
-  fileInputChange(event) {
-    this.selectedFiles = event.target.files;
-    this.showModal();
-  }
-
-  preventDefault(event) {
-    event.preventDefault();
   }
 }
